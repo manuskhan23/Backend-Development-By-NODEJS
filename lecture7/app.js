1c2ad8d064437c9a007a67f89c57d5e5ecb9d69d
Assalamalikum! Muhammad Anus Khan <muhammadanuskhan23@gmail.com>
2026-04-18 10:12:03 +0500

    Add files via upload

---
+++ lecture7/app.js
@@ -0,0 +1,10 @@
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+  console.log(req);
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on address http://localhost:${PORT}`);
+});
